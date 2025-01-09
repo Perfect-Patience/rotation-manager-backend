49cee445aabfdfa3d99f4def82cb7e571a38a020
Perfect Dah <patiencedah@gmail.com>
2025-01-09 23:56:35 +0000

    changes

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -12,26 +12,7 @@ export class RolesService {
   @InjectRepository(Role)
     private readonly roleRepository: Repository<Role> ){}
 
-    // private RolesSummary = [
-    //     {
-    //         "id": "cdbc",
-    //         "name": "Intern",
-    //         "count": "14",
-    //         "number_at_post": "5"
-    //       },
-    //       {
-    //         "id": "9843",
-    //         "name": "Trainee",
-    //         "count": "10",
-    //         "number_at_post": "3"
-    //       },
-    //       {
-    //         "id": "be3c",
-    //         "name": "National Service Person",
-    //         "count": "6",
-    //         "number_at_post": "2"
-    //       }
-    // ]
+    
 
     getRoles(){
         return this.roleRepository.find();
